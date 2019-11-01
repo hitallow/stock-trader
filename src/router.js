334a292bb660fd02ccdc3148318ae5f04b6c3b26
hitallow <hitallo91@gmail.com>
2019-11-01 18:38:41 -0300

    configuração de rotas

--- src/router.js
+++ src/router.js
@@ -1,17 +1,30 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import Home from './components/Home'
-import Portfolio from './components/portfolio/Portfolio'
-import Stocks from './components/stocks/Stocks'
+import Home from "./components/Home";
+import Portifolio from "./components/portifolio/Portifolio";
+import Stocks from "./components/stocks/Stocks";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
-    routes: [
-        { path: '/', component: Home },
-        { path: '/portfolio', component: Portfolio },
-        { path: '/stocks', component: Stocks },
-    ]
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: Home
+    },
+    {
+      path: "/portifolio",
+      component: Portifolio
+    },
+    {
+      path: "/stocks",
+      component: Stocks
+    },
+    {
+      path: "*",
+      redirect: "/"
+    }
+  ]
+});
